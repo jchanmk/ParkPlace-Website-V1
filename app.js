220742b54f6bfcdce91fac258d236e6425a6d458
Julian Chan <julianmkchan@gmail.com>
2019-01-24 00:01:52 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser");
+   // seedDB      = require("./seeds");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname))
+//seedDB();
+console.log(__dirname);
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("parq server has started");
+}); 
